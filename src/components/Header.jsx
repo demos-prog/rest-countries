3b49e787f8003755a343c8ce554fb419f5911cff
demos <demosne@gmail.com>
2021-12-12 15:20:52 +0300

    dfg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,23 @@ export default function Header({ toggleTheme, theme }) {
   return (
     <StyledHeader theme={theme}>
       <div className="wrapper">
-        <button onClick={() => toggleTheme()}>Change theme</button>
+        <StyledSpan theme={theme}>Where in the World ?</StyledSpan>
+        <StyledButtom theme={theme} onClick={() => toggleTheme()}>
+          Change theme
+        </StyledButtom>
       </div>
     </StyledHeader>
   );
 }
 
+const StyledSpan = styled.span`
+  color: ${(props) => (props.theme === true ? "black" : "white")};
+`;
+
 const StyledHeader = styled.header`
   background-color: ${(props) => (props.theme === true ? "white" : "#2B3743")};
+`;
+
+const StyledButtom = styled.button`
+  color: ${(props) => (props.theme === true ? "black" : "white")};
 `;
